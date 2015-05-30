1b41ad620985c4a7b2ed824ed998e1e3f68093cb
Vladislav Altanov <cactusabg@gmail.com>
2015-05-30 13:53:17 +0000

    Major refactoring and improvements

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<link rel="stylesheet" href="/css/reset.css" />
+<link rel="stylesheet" href="/css/Colaborate-fontfacekit/stylesheet.css" />
+<link rel="stylesheet" href="/css/style.css" />
+<link rel="stylesheet" href="/css/lightbox.css" type="text/css" />
+<!--[if IE]>
+    <link rel="stylesheet" type="text/css" href="css/IE.css" />
+<![endif]-->
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="/js/imgpreload.js" type="text/javascript"></script>
+<?php include('./includes/analytics.php');?>
