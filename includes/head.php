a1aa0081f8af89f14529b1b8c410ed528bb58851
Vladislav Altanov <cactusabg@gmail.com>
2015-12-19 14:37:14 +0000

    wip

--- includes/head.php
+++ includes/head.php
@@ -8,4 +8,4 @@
 <![endif]-->
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="/js/imgpreload.js" type="text/javascript"></script>
-<?php include('/includes/analytics.php');?>
+<?php include('analytics.php');?>
